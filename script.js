ed3fb7c32b494068ff657256b03434f10de7083b
Glitch (hello-webpage) <none>
2020-12-28 20:24:18 +0000

    🦄🖼 Checkpoint ./index.html:25158199/5826 ./css/menu.scss:25158199/4204 ./script.js:25158199/2267

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+// File#: _2_off-canvas-navigation
+// Usage: codyhouse.co/license
+(function() {
+  var OffCanvasNav = function(element) {
+    this.element = element;
+    this.panel = this.element.getElementsByClassName('js-off-canvas__panel')[0];
+    this.trigger = document.querySelectorAll('[aria-controls="'+this.panel.getAttribute('id')+'"]')[0];
+    this.svgAnim = this.trigger.getElementsByTagName('circle');
+    initOffCanvasNav(this);
+  };
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+  function initOffCanvasNav(canvas) {
+    if(transitionSupported) {
+      // do not allow click on menu icon while the navigation is animating
+      canvas.trigger.addEventListener('click', function(event){
+        canvas.trigger.style.setProperty('pointer-events', 'none');
+      });
+      canvas.panel.addEventListener('openPanel', function(event){
+        canvas.trigger.style.setProperty('pointer-events', 'none');
+      });
+      canvas.panel.addEventListener('transitionend', function(event){
+        if(event.propertyName == 'visibility') {
+          canvas.trigger.style.setProperty('pointer-events', '');
+        }
+      });
+    }
+
+    if(canvas.svgAnim.length > 0) { // create the circle fill-in effect
+      var circumference = (2*Math.PI*canvas.svgAnim[0].getAttribute('r')).toFixed(2);
+      canvas.svgAnim[0].setAttribute('stroke-dashoffset', circumference);
+      canvas.svgAnim[0].setAttribute('stroke-dasharray', circumference);
+      Util.addClass(canvas.trigger, 'offnav-control--ready-to-animate');
+    }
+    
+    canvas.panel.addEventListener('closePanel', function(event){
+      // if the navigation is closed using keyboard or a11y close btn -> change trigger icon appearance (from arrow to menu icon) 
+      if(event.detail == 'key' || event.detail == 'close-btn') {
+        canvas.trigger.click();
+      }
+    });
+  };
+
+  // init OffCanvasNav objects
+  var offCanvasNav = document.getElementsByClassName('js-off-canvas--nav'),
+    transitionSupported = Util.cssSupports('transition');
+  if( offCanvasNav.length > 0 ) {
+    for( var i = 0; i < offCanvasNav.length; i++) {
+      (function(i){new OffCanvasNav(offCanvasNav[i]);})(i);
+    }
+  }
+}());
