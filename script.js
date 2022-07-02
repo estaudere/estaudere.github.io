11f7353d20420b13c8a2cfc00bbd4bb7ef89ee96
neha <nehadesaraju@gmail.com>
2022-07-02 16:38:51 -0500

    edit font awesome

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-
-  $.getScript("", function(){
-
-    alert("Script loaded and executed.");
-    if (window.console) console.log('foo');
-    console.log(data);
-  });
-
+  $.getJSON("https://raw.githubusercontent.com/estaudere/estaudere.github.io/master/data.json", function(data) {
+    addSocials(data.socials);
+  })
 });
+
+function addSocials(socials) {
+  $.each(socials, function(i, val) {
+    $("#connect > ul").append("<li><a href='" + val.link + "'><i class='" + val.icon + "'></i>" + val.title + "</a></li>");
+  })
+}
