9385b3b3ffdf964d993caef535ca896782235636
neha <nehadesaraju@gmail.com>
2022-07-03 00:14:50 -0500

    js data injection script

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
     addSocials(data.socials);
     addCurrently(data.currently['2022'], '2022');
     addCurrently(data.currently['2021'], '2021');
+    addProjects(data.projects);
   })
 });
 
 function addSocials(socials) {
   $.each(socials, function(i, val) {
-    $("#connect > ul").append("<li><a href='" + val.link + "'><i class='" + val.icon + "'></i>" + val.title + "</a></li>");
+    $("#connect > ul").append("<li><a href='" + val.link + "' target='_blank' rel='noreferrer noopener'><i class='" + val.icon + "'></i>" + val.title + "</a></li>");
   })
 }
 
@@ -16,7 +17,7 @@ function addCurrently(currently_year, year) {
   $.each(currently_year, function(i, val) {
     text = val.title + "<span class='company'>" + val.company + "</span>";
     if (val.link) {
-      $("#" + year + " + ul").append("<li><a href='" + val.link + "'>" + text + "<i class='fa fa-arrow-right-long'></i></a></li>");
+      $("#" + year + " + ul").append("<li><a href='" + val.link + "' target='_blank' rel='noreferrer noopener'>" + text + "<i class='fa fa-arrow-right-long'></i></a></li>");
     }
     else {
       $("#" + year + " + ul").append("<li>" + text + "</li>");
@@ -30,13 +31,14 @@ function addProjects(projects) {
     gh_link = ""
 
     if (val['link']) {
-      link = `<a href=${val.link}><i class="fa fa-arrow-up-right-from-square></i></a>`;
+      link = `<a href="${val.link}" target="_blank" rel="noreferrer noopener"><i class="fa fa-arrow-up-right-from-square"></i></a>`;
+      console.log(link)
     }
     if (val['gh-link']) {
-      gh_link = `<a href=${val['gh-link']}><i class="fa-brands fa-github"></i></a>`;
+      gh_link = `<a href="${val['gh-link']}" target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-github"></i></a>`;
     }
 
-    text = `<li class='project'><h4>${val.title}${link}${gh_link}</h4><p>${val.description}</li>`;
+    text = `<li class='project'><h4>${val.title}${link}${gh_link}</h4><p class="date">${val.date}</p><p class="description">${val.description}</li>`;
     $("#projects > ul").append(text);
   })
 }
