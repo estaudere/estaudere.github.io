a6d7c36cc15e01d1c955e636b7e7e808429530e7
Neha <nehadesaraju@gmail.com>
2023-12-21 20:11:23 -0600

    fix styles not loading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
     import '../app.css'
     import Header from '$lib/components/Header.svelte'
     import SocialBar from '$lib/components/SocialBar.svelte'
+    import { browser } from '$app/environment'
 
     export const prerender = true;
 </script>
 
+{#if browser}
 <Header />
 
 <div class="extra-margin">
@@ -13,3 +15,4 @@
     <SocialBar />
 </div>
 <div class="h-24"></div>
+{/if}
