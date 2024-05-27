28f71a117faf5cada45c6435570930484da60893
Neha <nehadesaraju@gmail.com>
2024-05-27 13:54:05 -0700

    remove old website

--- _app/immutable/nodes/4.6A5exDKd.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.t1upX1mg.js";import{S as t,i as e}from"../chunks/index.5BUxBg9Z.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
